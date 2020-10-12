2fcb482bfcbd3c2a9aca973083c7e9e03c00e1b8
Nikita_K <nilkita.kom@sevencollab.com>
2020-10-12 17:30:30 +0300

    fix

--- src/product/product.service.spec.ts
+++ src/product/product.service.spec.ts
@@ -1,5 +1,5 @@
 import { getModelToken } from '@nestjs/mongoose';
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { CreateProductDTO } from './dto/create-product.dto';
 import { ProductService } from './product.service';
 
