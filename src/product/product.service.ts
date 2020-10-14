59367becba11fd06accd84273fdeb158cf0005d0
NikitaKompanetc <kni2012@ukr.net>
2020-10-14 14:44:38 +0300

    push (#3)
    Co-authored-by: Nikita_K <nilkita.kom@sevencollab.com>

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -15,6 +15,7 @@ export class ProductService {
   public async createProduct(
     createProductDto: CreateProductDTO,
   ): Promise<Product> {
+    //Goood one
     const productDocument = new this.productDocument(createProductDto);
     return await productDocument.save();
   }
